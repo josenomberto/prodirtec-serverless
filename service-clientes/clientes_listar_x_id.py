6c16d9830a204039024779eedb7924c8cac69b29
Jose Carlos Nomberto <jnombert@cisco.com>
2025-10-04 13:15:08 -0500

    Try separate .py for Update Client and List Cliente x ID

---
+++ service-clientes/clientes_listar_x_id.py
@@ -0,0 +1,22 @@
+import boto3
+import os
+import json
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    #table_name = os.environ.get('CLIENTS_TABLE_NAME', 'ClientsTable-dev') # Usar una variable de entorno
+    table_name = os.environ.get('CLIENTS_TABLE_NAME') # Usar una variable de entorno
+    clients_table = dynamodb.Table(table_name)
+    # Lee todos los registros
+    #response = clients_table.scan()
+    body = event['body']
+    response = clients_table.get_item(Key={'cliente_id': body.get('cliente_id')})
+    items = response['Items']
+    num_reg = response['Count']
+    # Salida (json)
+    return {
+        'statusCode': 200,
+        'body': items,
+        # 'body': json.dumps(items),
+        'headers': {'Content-Type': 'application/json'}
+    }
